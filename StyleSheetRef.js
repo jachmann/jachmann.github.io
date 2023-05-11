f8be1ade8be2522f1fd1bece78dae5fd44da25e9
jachmann <jachmann@coli.uni-saarland.de>
2023-05-11 15:04:19 +0200

    CoLi Copy

---
+++ StyleSheetRef.js
@@ -0,0 +1,10 @@
+var fileref = document.createElement("link");
+fileref.rel = "stylesheet";
+fileref.type = "text/css";
+fileref.href = "/typo3temp/assets/compressed/style-7da63c3b0cbd01214b1bca2034fd66d4.css.gzip?1683611304";
+fileref.media="all";
+document.getElementsByTagName("head")[0].appendChild(fileref);
+
+var scriptref = document.createElement("script");
+scriptref.src= "/typo3temp/assets/compressed/merged-2cc5759ea48b3223b5bdf560f4239c2d-18ec8e570360677187f6dbb5070212fc.js.gzip?1683090962";
+document.getElementsByTagName("head")[0].appendChild(scriptref);
